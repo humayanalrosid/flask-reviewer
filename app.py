0532cfd790f1d85b8eced8f2eb4f1ea522b23c47
Humayan Al Rosid <sony.leo.sony@gmail.com>
2023-06-25 15:04:09 +0530

    add gunicorn

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import requests, logging
 from flask import Flask, render_template, request, redirect
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as ul
-from flask_cors import CORS, cross_origin
-from pymongo.mongo_client import MongoClient
 
 logging.basicConfig(filename='scrapper.log', level=logging.INFO)
 
